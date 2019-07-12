2c5a6f0205bbd940471fb8e7db72e9dee020950b
Stephanie Patton <stephaniepatton@Stephanies-MacBook-Air.local>
2019-07-12 11:20:50 -0500

    fixed cityreader file, test pass

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -30,7 +30,7 @@ cities = []
 
 
 def cityreader(cities=[]):
-    # TODO Implement the functionality to read from the 'cities.csv' file
+    # Implement the functionality to read from the 'cities.csv' file
     # For each city record, create a new City instance and add it to the
     # `cities` list
     with open("cities.csv") as csvfile:
@@ -40,6 +40,32 @@ def cityreader(cities=[]):
     return cities
 
 
+cityreader(cities)
+
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
     print(c)
+
+# *** STRETCH GOAL! ***
+#
+# Allow the user to input two points, each specified by latitude and longitude.
+# These points form the corners of a lat/lon square. Output the cities that fall
+# within this square.
+#
+# Be aware that the user could specify either a lower-left/upper-right pair of
+# coordinates, or an upper-left/lower-right pair of coordinates. Hint: normalize
+# the input data so that it's always one or the other, then search for cities.
+#
+# Example I/O:
+#
+# Enter lat1,lon1: 45,-100
+# Enter lat2,lon2: 32,-120
+# Albuquerque: (35.1055,-106.6476)
+# Riverside: (33.9382,-117.3949)
+# San Diego: (32.8312,-117.1225)
+# Los Angeles: (34.114,-118.4068)
+# Las Vegas: (36.2288,-115.2603)
+# Denver: (39.7621,-104.8759)
+# Phoenix: (33.5722,-112.0891)
+# Tucson: (32.1558,-110.8777)
+# Salt Lake City: (40.7774,-111.9301)
